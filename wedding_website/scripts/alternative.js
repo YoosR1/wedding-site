9be903033ea76a0ad699ad8d011168c5ffad9907
Victoria <vfern124@fiu.edu>
2023-01-16 23:59:36 -0500

    Finished part 7 without the Copyright Section
    I did not want to include the copyright section so it was skipped

--- wedding_website/scripts/alternative.js
+++ wedding_website/scripts/alternative.js
@@ -46,4 +46,56 @@ $(document).ready(function() {
 	//gallery popup
 	const wHeight = $(window).height();
 	$(".gallery-popup img").css("max-height", wHeight + "px");
+
+	let itemIndex = 0;
+	const totalGalleryItems = $(".gallery-item").length;
+	
+	$(".gallery-item").click(function() {
+		itemIndex = $(this).index();
+		$(".gallery-popup").addClass("open");
+		$(".gallery-popup .gp-img").hide();
+		gpSlideShow();
+	})
+
+	$(".gp-controls .next").click(function() {
+		if (itemIndex == totalGalleryItems - 1) {
+			itemIndex = 0;
+		}
+		else {
+			itemIndex++;
+		}
+		$(".gallery-popup .gp-img").fadeOut(function() {
+			gpSlideShow();
+		})
+	})
+
+	$(".gp-controls .prev").click(function() {
+		if (itemIndex == 0) {
+			itemIndex = totalGalleryItems - 1;
+		}
+		else {
+			itemIndex--;
+		}
+		$(".gallery-popup .gp-img").fadeOut(function() {
+			gpSlideShow();
+		})
+	})
+
+	function gpSlideShow() {
+		const imgSrc = $(".gallery-item").eq(itemIndex).find("img").attr("data-large");
+		$(".gallery-popup .gp-img").fadeIn().attr("src", imgSrc);
+		$(".gp-counter").text((itemIndex + 1) + "/" + totalGalleryItems);
+	}
+
+	// close gallery popup
+	$(".gp-close").click(function() {
+		$(".gallery-popup").removeClass("open");
+	})
+
+	// hide gallery popup when clicked outside of gp-container
+	$(".gallery-popup").click(function(event) {
+		if($(event.target).hasClass("open")) {
+			$(".gallery-popup").removeClass("open");
+		}
+	})
 })
